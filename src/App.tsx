98cdc02d30788e833ba035a4888f9cf627c9d63a
Trịnh Hoàng Phúc <hoangphuc01022002@gmail.com>
2025-08-23 16:56:07 +0700

    feat: create befenits section

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
-import './App.css'
-import Banner from './components/banner'
-import Benefits from './components/benefits'
-import CoursesCategory from './components/courses-category'
-import CallToAction from './components/cta'
-import Footer from './components/footer'
-import Header from './components/header'
-import PopularCourses from './components/popular'
-import Slider from './components/slider'
-import Training from './components/training'
+import "./App.css";
+import Banner from "./components/banner";
+import Benefits from "./components/benefits";
+import CoursesCategory from "./components/courses-category";
+import CallToAction from "./components/cta";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import PopularCourses from "./components/popular";
+import Slider from "./components/slider";
+import Training from "./components/training";
 
 function App() {
-
   return (
     <>
       <Header />
       <Banner />
-      <PopularCourses />
-      <CoursesCategory />
-      <Benefits />
-      <Training />
-      <CallToAction />
-      <Slider />
+      <div className="space-y-28">
+        <PopularCourses />
+        <CoursesCategory />
+        <Benefits />
+        <Training />
+        <CallToAction />
+        <Slider />
+      </div>
       <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
