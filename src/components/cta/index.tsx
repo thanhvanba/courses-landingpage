3cf0afe6329308e9a980fbffd1c0db2edc6f9933
Trịnh Hoàng Phúc <hoangphuc01022002@gmail.com>
2025-08-23 23:45:26 +0700

    feat: add line_circle image to CallToAction component and adjust z-index for woman picture

--- src/components/cta/index.tsx
+++ src/components/cta/index.tsx
@@ -1,12 +1,20 @@
 import { MdStars } from "react-icons/md";
+import line_circle from "../../assets/images/cta/line_circle.png";
 import woman_picture from "../../assets/images/cta/woman.png";
+
 export default function CallToAction() {
   return (
     <section id="cta" className="container relative text-white lg:!mt-20">
       <img
         src={woman_picture}
         alt="Woman holding gift cards"
-        className="absolute bottom-0 right-0 w-[50%] lg:block hidden"
+        className="absolute bottom-0 right-0 w-[50%] lg:block hidden z-10"
+      />
+
+      <img
+        src={line_circle}
+        alt="Woman holding gift cards"
+        className="absolute bottom-0 left-1/2 -translate-x-1/2 lg:block hidden"
       />
       <div className="bg-[#DF1C25] rounded-[1rem] p-10 lg:px-16 px-10 w-full">
         <div className="lg:max-w-[60%] w-full">
